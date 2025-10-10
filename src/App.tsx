4f3a6c38d08aaa437ccc42224936c630b838f040
Erika <107076369+Erika0306@users.noreply.github.com>
2025-10-09 21:36:56 -0500

    Estructura de la interfaz principal en App.tsx con botones y temporizador

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,35 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div style={{position: 'relative'}}>
+  
+      <div>
+        <button className="closeButton">
+          Close
+        </button>
+      </div>
+
+      <div className="home-content">
+        <div className="home-controls">
+          <button className="image-button">
+            Work
+          </button>
+          <button className="imagene-button">
+            Break
+          </button>
+        </div>
+
+          <p>
+            You can do it!
+          </p>
+          <h1 className="home-timer"> 25:00</h1>
+          <button className="home-button">
+            Start
+          </button>
+      </div>
     </div>
+    
   );
 }
 
