9f273521faacd024e24b4ef22e7f708d1fe1c4dd
Erika <107076369+Erika0306@users.noreply.github.com>
2025-11-05 18:53:48 -0500

    Implemento toda la lógica del Pomodoro (temporizador, break y mensajes)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,73 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  const [timeLeft, setTimeLeft] = useState(25*60); //Un hook de react crear una variable con estado dentro de un componente funcional.
+  const [isRunning, setIsRunning] = useState(false); 
+  const [isBreak, setIsBreak] = useState(false);
+  const [encouragement,setEncouragement] = useState("");
+
+  const cheersMessages = [
+    "You can do it",
+    "Stay focus",
+    "You are amazing"
+  ];
+
+  const breakMessages = [
+    "Stay hydrated!",
+    "Snack, maybe?"
+  ];
+
+  // Menasjes 
+  useEffect(() =>{
+    let messageInterval: NodeJS.Timeout;
+    
+    if (isRunning){
+      const messages = isBreak ? breakMessages : cheersMessages;
+      setEncouragement(messages [0]); // El mesaje inicial 
+      let index = 1
+
+      messageInterval = setInterval(() => {
+        setEncouragement(messages[index]);
+        index = (index + 1) % messages.length;
+      }, 4000); //Cada cuatro segundos cabia el mensaje 
+    }
+    return () => clearInterval(messageInterval);
+  }, [isRunning,isBreak]);
+  // Cuenta regresiva 
+  useEffect ( () => { //Hook 
+    let timer: NodeJS.Timeout; //Almacenar el temporizador
+    if (isRunning && timeLeft > 0){
+      timer = setInterval( () => {
+        setTimeLeft(prev => prev - 1);
+      }, 1000); //Reduce los segundos 
+    }
+    return() => clearInterval(timer);
+  },[isRunning, timeLeft]);
+
+  const fromatTime = (seconds: number): string => {
+    const m = Math.floor(seconds / 60).toString().padStart(2,'0'); // padStart para que tenga 2 digitos 
+
+    const s = (seconds % 60).toString().padStart(2,'0');
+    return `${m}:${s}`; //Para devolver en terminos de reloj 
+  };
+
+  const switchMode = (breakMode: boolean) => {
+    setIsBreak(breakMode); // True Break False Work 
+    setIsRunning(false); // Para que el nuevo contador no corra de inmediato 
+    setTimeLeft(breakMode ? 5* 60 : 25* 60 );
+
+  }
+  
+  const handleClick = () => {
+  setIsRunning(!isRunning); // Cambia entre iniciar y pausar
+  };
+
+
   return(
     <div style={{position: 'relative'}}>
-  
       <div>
         <button className="closeButton">
           Close
@@ -14,19 +76,19 @@ function App() {
 
       <div className="home-content">
         <div className="home-controls">
-          <button className="image-button">
+          <button className="image-button" onClick={ () => switchMode(false)}>
             Work
           </button>
-          <button className="imagene-button">
+          <button className="imagene-button" onClick={ () => switchMode(true)}>
             Break
           </button>
         </div>
 
-          <p>
-            You can do it!
+          <p className={`encouragement-text ${!isRunning ? "hidden" : "" }`}>
+            {encouragement}
           </p>
-          <h1 className="home-timer"> 25:00</h1>
-          <button className="home-button">
+          <h1 className="home-timer">{fromatTime(timeLeft)}</h1>
+          <button className="home-button" onClick={handleClick}>
             Start
           </button>
       </div>
